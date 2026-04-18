62c3d1ec92437fbe563c681ced9340be40441f14
Rao Asad Mehmood <160304922+RaoAsadMehmood@users.noreply.github.com>
2026-04-19 03:29:19 +0500

    Add files via upload

---
+++ assignment-1.py
@@ -0,0 +1,55 @@
+# Q1 Aik variable a banao jisme value 20 ho, aur aik variable b banao jisme value 5 ho. In donon ko divide karo aur result print karo.
+
+a = 20
+b = 5
+print(a / b)
+
+# Q2 Aik variable name banao jisme apna naam likho. Usay print karo aur uska lower case bhi print karo.
+
+name = "SAAD NASEEM"
+print(name)
+print(name.lower())
+
+# Q3 Aik string likho: "hello world from python" Isay aise print karo ke har word ka pehla letter capital ho.
+
+text = "hello world from python"
+print(text.title())
+
+# Q4 Aik variable x = 10 aur y = 3 banao. Inka modulus (%) nikaal kar print karo.
+
+x = 10
+y = 3
+print(x % y)
+
+
+#Q5 Aik variable text = "PYTHON" banao. Isay lower case mai convert karke print karo
+
+text = "PYTHON"
+print(text.lower())
+
+# Q6 Aik string "data science" likho. Iski length nikaal kar print karo
+
+text = "data science"
+print(len(text))
+
+# Q7 Aik variable num = 7 banao. Iska square (power) nikaal kar print karo.
+
+num = 7
+print(num ** 2)
+
+
+# Q8 Aik string "hello" likho. Isay 3 dafa repeat karke print karo.
+
+text = "hello"
+print(text * 3)
+
+# Q9 Aik variable a = 15 aur b = 10 banao. Check karo ke a bara hai b se ya nahi aur result print karo
+
+a = 15
+b = 10
+print(a > b)
+
+# Q10 Aik string "i love python programming" likho. Isay aise print karo ke pehla letter capital ho (sirf first letter).
+
+text = "i love python programming"
+print(text.capitalize())
